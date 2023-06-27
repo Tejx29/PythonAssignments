21a04bfc031da86d24b6404066032a0ea8990c68
Tejx29 <137475515+Tejx29@users.noreply.github.com>
2023-06-27 20:32:19 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,72 @@
+# arithmetic operators
+a=10
+b=5
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+print("floor division: ",a//b)
+print("exponent: ",a**b)
+print("mod: ",a%b)
+
+# assignment operators
+a=10
+a+=1
+print("+=",a)
+a=10
+a-=1
+print("-=",a)
+a=10
+a*=1
+print("*=",a)
+a=10
+a/=1
+print("/=",a)
+a=10
+a**=1
+print("**=",a)
+a=10
+a//=1
+print("//=",a)
+a=10
+a%=1
+print("%=",a)
+
+# comparision operators / Relational operators
+a=10
+b=5
+print(">=",a>=b)
+print("<=",a<=b)
+print(">",a>b)
+print("<",a<b)
+print("==",a==b)
+print("!=",a!=b)
+
+#logical operators
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+#bitwise operators
+a=10
+b=5
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+
+
+#identity operators
+a=10
+b=5
+print("is",a is b)
+print("is not",a is not b)
+
+#membership operators
+a="tejas"
+print('t' in a)
+print("k" not in a)
+
