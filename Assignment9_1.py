466df35a63dfefe794442dca61c8f693104d4ac0
Tejx29 <137475515+Tejx29@users.noreply.github.com>
2023-07-03 22:55:58 +0530

    Add files via upload

---
+++ Assignment9_1.py
@@ -0,0 +1,27 @@
+# MathOperations.py
+
+class MathOperations:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        print("Addition: ",self.x + self.y)
+
+    def sub(self):
+        print("Subtraction: ",self.x - self.y)
+
+    def mul(self):
+        print("Multiplication: ",self.x * self.y)
+
+    def div(self):
+        if self.y == 0:
+            raise ZeroDivisionError
+        print("Division: ",self.x / self.y)
+
+    def pow(self):
+        print("Power: ",self.x ** self.y)
+        
+    def floorDiv(self):
+        print("Floor Division: ",self.x // self.y)
+
