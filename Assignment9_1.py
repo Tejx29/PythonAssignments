c54db710089e29c88bbc490ccc444827ba8061e4
Tejx29 <137475515+Tejx29@users.noreply.github.com>
2023-07-05 00:59:11 +0530

    Add files via upload

---
+++ Assignment9_1.py
@@ -0,0 +1,21 @@
+import math
+
+class MathOperations:
+    def sqrt(x):
+        return (math.sqrt(x))
+
+    def factorial(x):
+        return (math.factorial(x))
+
+    def sin(x):
+        return (math.sin(x))
+
+    def cos(x):
+        return (math.cos(x))
+    
+    def tan(x):
+        return (math.tan(x))
+
+    def log2(x):
+        return (math.log2(x))
+    
