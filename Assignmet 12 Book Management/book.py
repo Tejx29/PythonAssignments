38298c06ab8f13f969fd02fcb3272f807d84473f
Tejx29 <137475515+Tejx29@users.noreply.github.com>
2023-07-10 22:45:35 +0530

    Add files via upload

---
+++ Assignmet 12 Book Management/book.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
---
+++ Assignmet 12 Book Management/member.py
@@ -0,0 +1,4 @@
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
