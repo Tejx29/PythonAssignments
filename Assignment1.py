7dfc8a811b72b4f0e182b67e99fa0e64e97fcba6
Tejx29 <137475515+Tejx29@users.noreply.github.com>
2023-07-26 21:34:07 +0530

    Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,7 +1,8 @@
-n1 = 10
-n2 = 20
-n3 = 30
+n1 = int(input(('Enter the value of n1: ')))
+n2 = int(input(('Enter the value of n2: ')))
+n3 = int(input(('Enter the value of n3: ')))
+result = 0
 
 average = (n1 + n2 + n3) / 3
 
-print("The average of n1,n2 and n3 is",average)
+print("The average of",n1,",",n2,"and",n3,"is",average)
